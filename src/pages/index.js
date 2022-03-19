2affd845c929ee9cdc2d2c685448d08f4b55c5d8
Avery Durrant <avery246813579@gmail.com>
2022-03-19 00:19:09 -0400

    new setup with tailwind

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+class PagesIndex extends Component {
+  render() {
+    return (
+      <h1 className="text-3xl font-bold underline text-green-90">
+        Hello
+      </h1>
+    )
+  }
+}
+
+export default PagesIndex;
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "green-90": "#00db96",
+        "purple-90": "#49297e",
+        "blue-90": "#90dcff",
+        "pink-90": "#e10086",
+        "yellow-90": "#fdfb76",
+        "off-white": "#FAF9F6",
+        "off-black": "#30363a",
+      },
+    },
+  },
+  plugins: [],
+}
